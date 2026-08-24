afd7fbfffd872b167bd394e33c91c18232035d70
Raymond Coetzee <coetzeer@gmail.com>
2026-08-24 05:01:26 +0200

    Update clixad to 0.0.1-beta.12

--- Formula/clixad.rb
+++ Formula/clixad.rb
@@ -1,8 +1,8 @@
 class Clixad < Formula
   desc "Free AI coding agent in your terminal, funded by rewarded ads"
   homepage "https://clixad.io/"
-  url "https://registry.npmjs.org/clixad/-/clixad-0.0.1-beta.11.tgz"
-  sha256 "f6a69c8d637d8e9e816cf411f5de2f721fd3e2c7a08d44f7a402584dbb48601d"
+  url "https://registry.npmjs.org/clixad/-/clixad-0.0.1-beta.12.tgz"
+  sha256 "0314dcdbaf7e0c1b538b3ddd5970d73dd90985c691f21e443b301a615f45c945"
   license "Unlicense"
 
   depends_on "node@22"
